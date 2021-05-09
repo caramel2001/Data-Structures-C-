28d507564e4f104a7825ee128c4da7b99475f724
Pratham Agarwala <agarwalpratham2001@gmail.com>
2021-05-10 04:14:36 +0800

    Uploaded

---
+++ Hashing and Hashtables/DoubleHashing.c
@@ -0,0 +1,170 @@
+    #include <stdio.h>
+#include <stdlib.h>
+
+#define TABLESIZE 37
+#define PRIME     13
+
+enum Marker {EMPTY,USED,DELETED};
+
+typedef struct _slot{
+    int key;
+    enum Marker indicator;
+} HashSlot;
+
+int HashInsert(int key, HashSlot hashTable[]);
+int HashDelete(int key, HashSlot hashTable[]);
+
+
+int hash1(int key);
+int hash2(int key);
+
+int main()
+{
+    int opt;
+    int i;
+    int key;
+    int comparison;
+    HashSlot hashTable[TABLESIZE];
+
+    for(i=0;i<TABLESIZE;i++){
+        hashTable[i].indicator = EMPTY;
+        hashTable[i].key = 0;
+    }
+
+    printf("============= Hash Table ============\n");
+    printf("|1. Insert a key to the hash table  |\n");
+    printf("|2. Delete a key from the hash table|\n");
+    printf("|3. Print the hash table            |\n");
+    printf("|4. Quit                            |\n");
+    printf("=====================================\n");
+    printf("Enter selection: ");
+    scanf("%d",&opt);
+    while(opt>=1 && opt <=3){
+        switch(opt){
+        case 1:
+            printf("Enter a key to be inserted:\n");
+            scanf("%d",&key);
+            comparison = HashInsert(key,hashTable);
+            if(comparison <0)
+                printf("Duplicate key\n");
+            else if(comparison < TABLESIZE)
+                printf("Insert: %d Key Comparisons: %d\n",key, comparison);
+            else
+                printf("Key Comparisons: %d. Table is full.\n",comparison);
+            break;
+        case 2:
+            printf("Enter a key to be deleted:\n");
+            scanf("%d",&key);
+            comparison = HashDelete(key,hashTable);
+            if(comparison <0)
+                printf("%d does not exist.\n", key);
+            else if(comparison <= TABLESIZE)
+                printf("Delete: %d Key Comparisons: %d\n",key, comparison);
+            else
+                printf("Error\n");
+            break;
+        case 3:
+            for(i=0;i<TABLESIZE;i++) printf("%d: %d %c\n",i, hashTable[i].key,hashTable[i].indicator==DELETED?'*':' ');
+            break;
+        }
+        printf("Enter selection: ");
+        scanf("%d",&opt);
+    }
+    return 0;
+}
+
+int hash1(int key)
+{
+    return (key % TABLESIZE);
+}
+
+int hash2(int key)
+{
+    return (key % PRIME) + 1;
+}
+
+int HashInsert(int key, HashSlot hashTable[])
+{
+   //Write your code here
+   int index;
+   int temp;
+   int j;
+   int i=0;
+   index=hash1(key);
+   temp=hash2(key);
+    
+   if (hashTable[index].indicator == EMPTY)
+   {
+       hashTable[index].key = key;
+       hashTable[index].indicator = USED;
+       return i;
+   }
+   while(hashTable[index].indicator == USED){
+       
+       //increase index value by the value of second hash function
+       if(hashTable[index].key == key){
+           return -1;
+       }
+       index += temp;
+       index = index % TABLESIZE;
+       i++;
+       if (i>=TABLESIZE){
+           return TABLESIZE;
+       }
+   }
+   if(hashTable[index].indicator==EMPTY){
+       hashTable[index].key=key;
+       hashTable[index].indicator=USED;
+       return i;
+   }
+   else{        
+       j=index;
+       j += temp;
+       j = j % TABLESIZE;
+       while(hashTable[j].indicator!=EMPTY && i<TABLESIZE-1){
+            if(hashTable[j].indicator==USED){
+               i++;
+               if(hashTable[j].key == key){
+                   return -1;
+               }
+            }
+            j += temp;
+            j = j % TABLESIZE;     
+       }
+       hashTable[index].key=key;
+       hashTable[index].indicator=USED;
+       return i;
+   }
+}
+
+int HashDelete(int key, HashSlot hashTable[])
+{
+   //Write your code here
+   int index;
+   int temp;
+   int i=0;
+   index=hash1(key);
+   temp=hash2(key); 
+
+   while(hashTable[index].indicator != EMPTY)
+   {
+       if(hashTable[index].indicator==USED && hashTable[index].key==key){
+           hashTable[index].indicator=DELETED;
+           i++;
+           return i;
+       }
+       if (hashTable[index].indicator==DELETED){
+           index += temp;
+           index = index % TABLESIZE;
+           continue;
+       }
+       index += temp;
+       index = index % TABLESIZE;
+       i++;
+       if (i>=TABLESIZE){
+           return -1;
+       }
+
+   }
+   return -1;  
+}
